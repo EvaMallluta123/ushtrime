9e6f93606e7b25d43f72faf1ffb8fd19eec87976
Eva Malluta <eva.malluta14@gmail.com>
2022-07-15 08:58:20 +0200

    change

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-document.body.textContent = "";
+// document.body.textContent = "";
 console.log("Good luck Eva!");
 
 type Post={
@@ -13,19 +13,24 @@ type Coment= {
     postId: number;
 }
 type Profile= {
+    id: number
     name: string;
+    foodOrder: string
 }
 type State={
     post:Post[]
     coment:Coment[]
-    profile: Profile
+    profile: Profile[]
 }
 let state:State={
   post: [] ,
   coment:[],
-  profile: {
-    name: "Eva"
-  }
+  profile:[ {
+    id: 1,
+    name: "Eva",
+    foodOrder: "Hamburger"
+
+  }]
 
 
 }
@@ -44,19 +49,48 @@ let state:State={
 
 // - 1. connect to https://62ce7d27826a88972dfc73ca.mockapi.io/orders:
 //   - we should fetch the people from the server when the app starts
-function fetchPeopleFromServer (){
+function GetPeopleFromServer (){
 fetch("http://localhost:3005/profile")
 .then(resp=>resp.json())
 .then(person=>{
     state.profile=person
 })
 }
+function CreateNewOrder (name:string, foodOrder:string){
+    let url = 'http://localhost:3005/profile'
+
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    body: JSON.stringify({
+        name:name,
+        footOrder:foodOrder
+    })
+}
+fetch(url, options)
+.then(resp=>resp.json())
+.then(NewOrder=>{
+    state.profile.push(NewOrder)
+    render()
+})
+}
 function render(){
-    
+    let mainEl=document.querySelector("main")
+    if(mainEl===null)return
+    mainEl.textContent=""
+let pEl=document.createElement("p")
+for(let profil of state.profile) {
+pEl.textContent=`Name: ${profil.name}   FoodOrder:${profil.foodOrder}`
+}
+mainEl.append(pEl)
+
+
 }
 //   - when adding a person to the queue, both the app and the server should get updated
 // - 2. build counters at the top of the page for:
 //    - how many people are in the queue?
 //    - how many have already been served since we last refreshed the page?
-
-fetchPeopleFromServer ()
+render()
+GetPeopleFromServer ()
